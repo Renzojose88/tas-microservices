0a4cc6074a57cd6b0e70d2328e7c845714f6fef8
renzo <renzo03.88@gmail.com>
2023-03-03 22:11:35 -0500

    segundo commit

--- task-microservice/src/main/java/com/app/taskmicroservice/service/TaskService.java
+++ task-microservice/src/main/java/com/app/taskmicroservice/service/TaskService.java
@@ -2,12 +2,17 @@ package com.app.taskmicroservice.service;
 
 
 import com.app.taskmicroservice.entity.Task;
+import com.app.taskmicroservice.exceptions.ToDoException;
 import com.app.taskmicroservice.repository.TaskRepository;
+import org.aspectj.bridge.IMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
+import java.util.TooManyListenersException;
 
 @Service
 public class TaskService {
@@ -28,6 +33,21 @@ public class TaskService {
 
     public void deleteTask(Long idTask){
         taskRepository.deleteById(idTask);
+    }
+
+    public Optional<Task> getTaskById(Long idTask){
+        return taskRepository.findById(idTask);
+    }
+
+    public Task updateTask(Long idTask, Task taskUpdate){
+        Optional<Task> task = taskRepository.findById(idTask);
+        if(task.isEmpty()){
+            throw new ToDoException("Task no found", HttpStatus.NOT_FOUND);
+        }
+        taskUpdate.setId(idTask);
+        taskUpdate.setCreateDate(task.get().getCreateDate());
+        taskUpdate.setActive(task.get().isActive());
+        return taskRepository.save(taskUpdate);
     }
 
 }
